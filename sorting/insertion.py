4bb5022365f11fa652f53eb64914655675ba2cf6
Samuel Mendenhall <smendenh@redhat.com>
2016-02-27 16:53:21 -0500

    Insertion python

---
+++ sorting/insertion.py
@@ -0,0 +1,14 @@
+def insertion(arr):
+    for i in range(1, len(arr)):
+        ctr = i
+        while ctr > 0 and arr[ctr] < arr[ctr - 1]:
+            temp = arr[ctr - 1]
+            arr[ctr - 1] = arr[ctr]
+            arr[ctr] = temp
+            ctr -= 1
+
+
+if __name__ == '__main__':
+    arr = [8, 9, 7, 5, 2, 4, 6, 1, 3]
+    insertion(arr)
+    assert arr == [1, 2, 3, 4, 5, 6, 7, 8, 9]
