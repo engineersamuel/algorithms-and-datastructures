a23ca82ac63443e3b005fc59103c542e77706728
Samuel Mendenhall <smendenh@redhat.com>
2016-03-15 20:18:56 -0400

    adding Sequential Search Symbol Table in java

---
+++ datastructures/symboltables/SequentialSearchST.java
@@ -0,0 +1,43 @@
+public class SequentialSearchST<Key, Value> {
+
+    private Node first;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node next;
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return x.val;
+        }
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+    }
+
+    public static void main(String[] args) {
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<>();
+        st.put("a", 0);
+        st.put("b", 1);
+        st.put("c", 2);
+
+        System.out.println("a: " + st.get("a"));
+        System.out.println("a: " + st.get("b"));
+        System.out.println("a: " + st.get("c"));
+    }
+}
